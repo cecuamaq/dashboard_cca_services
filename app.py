29ca38ea385a600110ef8f3163b8e777542c53ad
cecuamaq <cecuamaq@gmail.com>
2026-03-09 20:45:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2759,4 +2759,5 @@ def serve_pdf(filename):
 server = app.server
 
 if __name__ == "__main__":
-    app.run(debug=False, port=8050)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port)
